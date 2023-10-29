350909b3d5fd55f15dc9036782256355d029c945
TranThiThuHuyen17A2DHKL <148513395+huyn-2410@users.noreply.github.com>
2023-10-29 22:11:37 +0700

    Add files via upload

---
+++ tinhtoan_ver_2.py
@@ -0,0 +1,17 @@
+#chuong trinh thu tu
+#tinh toan so hoc
+print('xin mời nhập số liệu')
+x= float(input("nhập giá trị x:6"))
+y= float(input("nhập giá trị y:10"))
+tong=x+y
+hieu=x-y
+tich=x*y
+thuong=x/y
+print("tổng của hai số ",x,"+",y,"=",tong)
+print("hiệu của hai số",x,"-",y,"=",hieu)
+print("tích của hai số",x,"*",y,"=", tich)
+print("thương của hai số",x,"/",y,"=",thuong)
+
+import math
+can_bac_hai_x=math.sprt(x)
+print("căn bậc hai của x",x,"=",can_bac_hai_x)
